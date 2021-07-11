a0586e59832c17a656ff37fe824bac7d377a0cf7
unknown <viniciusguimaraes2533@gmail.com>
2021-07-11 09:26:41 -0300

    second commit

--- app.js
+++ app.js
@@ -30,31 +30,31 @@ const coloredFont = document.querySelector('.colored-font')
 
 swiper.on('activeIndexChange', function () {
 
-  if(this.activeIndex === 0){
+  if (this.activeIndex === 0) {
     coloredFont.style.color = '#fdbb04'
   }
 
 
- if(this.activeIndex === 1){
-   coloredFont.style.color = '#000000'
- }
+  if (this.activeIndex === 1) {
+    coloredFont.style.color = '#000000'
+  }
 
- if(this.activeIndex === 2){
-  coloredFont.style.color = '#D2B293'
-}
+  if (this.activeIndex === 2) {
+    coloredFont.style.color = '#D2B293'
+  }
 
-if(this.activeIndex === 3){
-  coloredFont.style.color = '#8C2753'
-}
+  if (this.activeIndex === 3) {
+    coloredFont.style.color = '#8C2753'
+  }
 
 
-if(this.activeIndex === 4){
-  coloredFont.style.color = '#ECDBB5'
-}
+  if (this.activeIndex === 4) {
+    coloredFont.style.color = '#ECDBB5'
+  }
 
-if(this.activeIndex === 5){
-  coloredFont.style.color = '#090909'
-}
+  if (this.activeIndex === 5) {
+    coloredFont.style.color = '#090909'
+  }
 
 })
 
